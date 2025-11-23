c60c67a65fb44f9714ced11949a935efa983ef29
quaid-perkins <quaidperkins@gmail.com>
2025-11-23 14:26:20 +0000

    style: restructure index hero section

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,63 +5,63 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
-	head: () => ({
-		meta: [
-			{
-				charSet: "utf-8",
-			},
-			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{
-				title: "Void Modding",
-			},
-		],
-		links: [
-			{
-				rel: "stylesheet",
-				href: appCss,
-			},
-		],
-	}),
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Void Modding",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
 
-	shellComponent: RootDocument,
+  shellComponent: RootDocument,
 });
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<HeadContent />
-				<meta name="og:title" content="Void modding" />
-				<meta name="og:description" content="The new home of modding (maybe)" />
-				<meta name="og:url" content="https://voidmodding.org" />
-				<meta name="og:image" content="/preview.png" />
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+        <meta name="og:title" content="Void modding" />
+        <meta name="og:description" content="The new home of modding (maybe)" />
+        <meta name="og:url" content="https://voidmodding.org" />
+        <meta name="og:image" content="/preview.png" />
 
-				<meta name="twitter:title" content="Void modding" />
-				<meta
-					name="twitter:description"
-					content="The new home of modding (maybe)"
-				/>
-				<meta name="twitter:image" content="/preview.png" />
-				<meta name="twitter:url" content="https://voidmodding.org" />
-			</head>
-			<body>
-				{children}
-				<TanStackDevtools
-					config={{
-						position: "bottom-right",
-					}}
-					plugins={[
-						{
-							name: "Tanstack Router",
-							render: <TanStackRouterDevtoolsPanel />,
-						},
-					]}
-				/>
-				<Scripts />
-			</body>
-		</html>
-	);
+        <meta name="twitter:title" content="Void modding" />
+        <meta
+          name="twitter:description"
+          content="The new home of modding (maybe)"
+        />
+        <meta name="twitter:image" content="/preview.png" />
+        <meta name="twitter:url" content="https://voidmodding.org" />
+      </head>
+      <body data-theme="system">
+        {children}
+        <TanStackDevtools
+          config={{
+            position: "bottom-right",
+          }}
+          plugins={[
+            {
+              name: "Tanstack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,94 +1,98 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { DownloadIcon, GithubIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return (
-		<div className="h-screen overflow-y-auto scroll-smooth bg-[#0f1115] text-white font-sans">
-			<header className="sticky top-0 z-50 flex justify-between items-center px-8 py-6 bg-[#0f1115]/90 backdrop-blur border-b border-white/5">
-				<div className="flex items-center gap-2">
-					<img src="/logo512.png" alt="Void Mod Manager" className="h-10" />
-					<span className="text-sm opacity-80">Void Mod Manager</span>
-				</div>
-				<nav>
-					<a
-						href="https://github.com/void-modding/web"
-						className="text-sm opacity-80 hover:opacity-100 transition"
-					>
-						Documentation
-					</a>
-					{/*We need to add the docs page, github has a wiki we could use that*/}
-				</nav>
-			</header>
+  return (
+    <div className="h-screen overflow-y-auto scroll-smooth bg-[#0f1115] font-sans text-white">
+      <header className="sticky top-0 z-50 flex items-center justify-between border-white/5 border-b bg-[#0f1115]/90 px-8 py-6 backdrop-blur">
+        <div className="flex items-center gap-2">
+          <img src="/logo512.png" alt="Void Mod Manager" className="h-10" />
+          <span className="text-sm opacity-80">Void Mod Manager</span>
+        </div>
+        <nav>
+          <a
+            href="https://github.com/void-modding/web"
+            className="text-sm opacity-80 transition hover:opacity-100"
+          >
+            Documentation
+          </a>
+          {/*We need to add the docs page, github has a wiki we could use that*/}
+        </nav>
+      </header>
 
-			<main className="flex flex-col items-center text-center mt-10 px-4">
-				<img src="/logo512.png" alt="Logo" className="h-50" />
-				<h1 className="text-4xl font-bold mb-2">
-					The new <span className="text-cyan-400">home</span> of Modding
-				</h1>
-				<p className="opacity-70 max-w-md mb-6">
-					Welcome to the new home of Modding for any game, fully Open-Sourced.
-				</p>
+      <main className="flex flex-col items-center gap-16 py-16 text-center">
+        <div className="flex flex-col items-center gap-6">
+          <img src="/logo512.png" alt="Logo" className="h-50" />
+          <div className="flex flex-col items-center gap-3">
+            <h1 className="font-bold text-5xl">
+              The new <span className="text-cyan-400">home</span> of Modding
+            </h1>
+            <p className="mb-6 max-w-md opacity-70">
+              Welcome to the new home of Modding for any game, fully
+              Open-Sourced.
+            </p>
+          </div>
+        </div>
 
-				<div className="flex gap-4 mb-20">
-					<a
-						href="/download"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="bg-cyan-500 hover:bg-cyan-600 px-6 py-2 rounded-md font-medium transition inline-flex items-center gap-2"
-					>
-						<DownloadIcon size={20} />
-						Download
-					</a>
-					<a
-						href="https://github.com/void-modding/"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="bg-[#1a1d23] hover:bg-[#22262d] px-6 py-2 rounded-md font-medium transition border border-white/10 inline-flex items-center gap-2"
-					>
-						<GithubIcon size={20} />
-						Source Code
-					</a>
-				</div>
-				<section className="w-full max-w-5xl flex flex-col md:flex-row items-center justify-between mt-20">
-					<div className="max-w-lg text-left">
-						<h2 className="text-3xl font-semibold mb-2">
-							Find what you need,
-							<br />
-							<span className="text-cyan-400">whenever you need</span>
-						</h2>
-						<p className="opacity-70 text-sm">
-							Search through thousands of mods spanning across hundreds of games
-							instantly. Filter by category, popularity and more.
-						</p>
-					</div>
+        <div className="mb-20 flex gap-4">
+          <Button variant="primary" size="lg" asChild>
+            <a href="/download" target="_blank" rel="noopener noreferrer">
+              <DownloadIcon />
+              Download
+            </a>
+          </Button>
+          <Button size="lg" asChild>
+            <a
+              href="https://github.com/void-modding/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubIcon />
+              Source Code
+            </a>
+          </Button>
+        </div>
+        <section className="mt-20 flex w-full max-w-5xl flex-col items-center justify-between md:flex-row">
+          <div className="max-w-lg text-left">
+            <h2 className="mb-2 font-semibold text-3xl">
+              Find what you need,
+              <br />
+              <span className="text-cyan-400">whenever you need</span>
+            </h2>
+            <p className="text-sm opacity-70">
+              Search through thousands of mods spanning across hundreds of games
+              instantly. Filter by category, popularity and more.
+            </p>
+          </div>
 
-					<div>
-						<img src="Placeholder.png"></img>
-						{/* I guess we adding an image here */}
-					</div>
-				</section>
+          <div>
+            <img src="Placeholder.png"></img>
+            {/* I guess we adding an image here */}
+          </div>
+        </section>
 
-				<section className="w-full max-w-5xl flex flex-col md:flex-row items-center justify-between mt-20">
-					<div>
-						<img src="Placeholder.png"></img>
-						{/* I guess we adding an image here */}
-					</div>
+        <section className="mt-20 flex w-full max-w-5xl flex-col items-center justify-between md:flex-row">
+          <div>
+            <img src="Placeholder.png"></img>
+            {/* I guess we adding an image here */}
+          </div>
 
-					<div className="max-w-lg text-left">
-						<h2 className="text-3xl font-semibold mb-2">
-							Automaticlly installs
-							<br />
-							<span className="text-cyan-400">for you</span>
-						</h2>
-						<p className="opacity-70 text-sm">
-							Automaticly installs mods for you with a single click. No more
-							manual installs, no more broken mods.
-						</p>
-					</div>
-				</section>
-			</main>
-		</div>
-	);
+          <div className="max-w-lg text-left">
+            <h2 className="mb-2 font-semibold text-3xl">
+              Automaticlly installs
+              <br />
+              <span className="text-cyan-400">for you</span>
+            </h2>
+            <p className="text-sm opacity-70">
+              Automaticly installs mods for you with a single click. No more
+              manual installs, no more broken mods.
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
