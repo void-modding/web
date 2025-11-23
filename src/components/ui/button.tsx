6b498bc131056c5d94ebf4312ec0af3bfc21e598
quaid-perkins <quaidperkins@gmail.com>
2025-11-23 14:07:22 +0000

    feat: button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,58 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import type * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex shrink-0 cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium text-sm outline-none transition-all focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-slate-200 text-slate-950 shadow-xs hover:bg-slate-200/90 dark:bg-slate-700 dark:text-slate-50 dark:hover:bg-slate-700/90",
+        primary: "bg-primary text-slate-950 hover:bg-primary/90",
+        outline:
+          "border-2 border-slate-300 bg-slate-100 text-slate-950 shadow-xs hover:bg-slate-100/90 dark:border-slate-600 dark:bg-slate-700 dark:text-slate-50 dark:hover:bg-slate-700/90",
+        ghost:
+          "text-slate-950 hover:bg-slate-200 dark:text-slate-50 dark:hover:bg-slate-700",
+        link: "text-slate-950 underline-offset-4 hover:underline dark:text-slate-50",
+      },
+      size: {
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 px-6 text-base has-[>svg]:px-4",
+        icon: "size-9",
+        "icon-sm": "size-8",
+        "icon-lg": "size-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };
